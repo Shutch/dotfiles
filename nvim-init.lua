c6cdff57b41f9a6d235f6d033c749eea152ff181
Shutch <shutchinson1190@gmail.com>
2022-07-14 12:07:14 +1000

    More bootstrapping fixes

--- nvim-init.lua
+++ nvim-init.lua
@@ -19,7 +19,7 @@ local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
+  vim.api.nvim_command('packadd packer.nvim')
 end
 
 return require('packer').startup(function(use)
